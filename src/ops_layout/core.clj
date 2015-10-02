bd2e81ba7de58cb68893a2c8ced11da8295c67f8
Tom Marsh <ts.marsh@gmail.com>
2015-10-02 19:28:02 -0400

    Ring server

--- src/ops_layout/core.clj
+++ src/ops_layout/core.clj
@@ -1,6 +1,25 @@
-(ns ops-layout.core)
+(ns ops-layout.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [hiccup.core :as h]
+            [org.httpkit.server :as hk]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app
+  (GET "/" [] (h/html [:h1 "Hello, Fatass!"]))
+  (route/not-found (h/html [:h1 "Dumbass"])))
+
+(defonce server (atom nil))
+
+(def port (atom 8888))
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)
+    (println "Server stopped")))
+
+(defn start-server []
+  (reset! server (hk/run-server #'app {:port @port}))
+  (println "Server running on" @port))
