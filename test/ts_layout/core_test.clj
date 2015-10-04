f018ec2818220cb2b2f22bd6c6083669145c4af2
Tom Marsh <ts.marsh@gmail.com>
2015-10-03 20:51:09 -0400

    Adding tests for routes part 1

--- test/ts_layout/core_test.clj
+++ test/ts_layout/core_test.clj
@@ -2,4 +2,10 @@
   (:require [clojure.test :refer :all]
             [ts-layout.core :refer :all]))
 
+(defn request [resource web-app & params]
+   (web-app {:request-method :get :uri resource :params (first params)}))
 
+(deftest root
+  (let [response (request "/" app)]
+    (is (= 302 (:status response)))
+    (is (= {"Location" "/boxes"} (:headers response)))))
